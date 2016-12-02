00a449953984e00cf61ef646f8662a7938ca402a
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-12-02 01:18:59 -0500

    add vue and bootstrap

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-var finalTable = $('table');
-$('form').submit(function(){
-  var str= ""
-  input.value.split(',').forEach(function(e){
-    str += finalTable.find('td:contains('+e+')').parent().text();
-
-  })
-  result.innerText = str;
-  return false;
-});
+var app = new Vue({
+  el: "#app",
+  data: {
+    finals: finals,
+    courseInput: ""
+  },
+  computed: {
+    choice: function () {
+      return this.courseInput.toUpperCase().split(/[^A-Z0-9]+/)
+    },
+    filteredFinals: function () {
+      return this.finals.filter(final =>
+        this.choice.some(str => final.id.indexOf(str) == 0)
+      )
+    }
+  }
+})
