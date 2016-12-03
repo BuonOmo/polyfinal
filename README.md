fddf7d238d58499109b394ed22cf6d44aa457aa0
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-12-03 18:16:30 -0500

    Fix #1 (fonctionnalité d’export d’agenda)
    Now exporting, but result has not been tested on an ics agenda

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # polyfinal
 Récupère les dates des examens finaux à Polytechnique montreal
+
+Le dossier lib est une série de hardlink permettant de garder l’avantage des node_modules et d’avoir les dépendances pour la gh-page (qui n’installe pas les node_modules).
--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
-var storagePrefix="POLYFINAL_"
+/**
+ * Prefix used for localStorage, change it if you’re in dev and want to keep your saved data.
+ * @type {String}
+ */
+const STORAGE_PREFIX="POLYFINAL_";
+/**
+ * Format to use to parse date from finals table with moment(string, DATE_FORMAT).
+ * @type {String}
+ */
+const DATE_FORMAT = "D MMMM HH:mm";
+
+
 var app = new Vue({
   el: "#app",
   data: {
     finals: finals,
-    courseInput: localStorage.getItem(storagePrefix+"courseInput") || "",
-    name: localStorage.getItem(storagePrefix+"name"),
+    courseInput: localStorage.getItem(STORAGE_PREFIX+"courseInput") || "",
+    name: localStorage.getItem(STORAGE_PREFIX+"name"),
   },
   computed: {
     choice: function () {
@@ -14,6 +25,9 @@ var app = new Vue({
       return this.finals.filter(final =>
         this.choice.some(str => final.id.indexOf(str) == 0) &&(!this.name || this.nameInRange(final.names))
       )
+    },
+    ICSurl: function () {
+      return URL.createObjectURL(new File([this.exportICS()],"agenda-finaux.ics"));
     }
   },
   methods: {
@@ -22,8 +36,24 @@ var app = new Vue({
                               range[1].lastname.toUpperCase() > this.name.toUpperCase()
     },
     save: function() {
-      if (this.courseInput) localStorage.setItem(storagePrefix+"courseInput", this.courseInput);
-      if (this.name) localStorage.setItem(storagePrefix+"name", this.name);
+      if (this.courseInput) localStorage.setItem(STORAGE_PREFIX+"courseInput", this.courseInput);
+      if (this.name) localStorage.setItem(STORAGE_PREFIX+"name", this.name);
+    },
+    exportICS: function() {
+      var ics = new ICAL.Component(['vcalendar',[],[]]);
+      this.filteredFinals.map(final => {
+        var vevent = new ICAL.Component('vevent'),
+            event = new ICAL.Event(vevent);
+        var dtstart = moment(final.date, DATE_FORMAT);
+        event.startDate = ICAL.Time.fromString(dtstart.format());
+        event.endDate   = ICAL.Time.fromString(dtstart.add({hour: 3}).format());
+        event.summary   = "Final de "+final.id+".";
+        event.location  = "Polytechnique montréal, salle "+final.class;
+        event.uid = "POLY_FINAL_"+final.id+final.section;
+        vevent.addPropertyWithValue('dtstamp', ICAL.Time.now());
+        ics.addSubcomponent(vevent);
+      });
+      return ics.toString();
     }
   }
 })
